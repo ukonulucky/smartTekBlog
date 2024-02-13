3229b9b41a076be2adb67a58c1dcb5ea6aa6efb5
Ukonu Lucky Ifeanyichukwu <48377220+ukonulucky@users.noreply.github.com>
2024-02-13 22:07:42 +0100

    controller

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 require("dotenv").config()
 const express = require('express');
 const cors = require('cors')
+const asyncHandler = require("express-async-handler")
 
 // third party libraries
 
 
 // form my application
 const dbConnetFunc = require('./config/db/dbConnect');
-const PostModel = require("./model/Post/Post")
+const PostModel = require("./model/Post/Post");
+const expressAsyncHandler = require("express-async-handler");
 
 // form my application
 
@@ -31,20 +33,23 @@ app.use(cors(corsOptions))
 
 
 // creating post
-app.post("/api/v1/posts/create", async (req, res) => {
+app.post("/api/v1/posts/create", asyncHandler(async (req, res, next) => { 
     
-     try{
-     const createdPost = await PostModel.create(req.body)
-         return res.json({
-            status:"success",
-            message:"Post created successfull",
-            createdPost
-         })
-     }catch(error){
-          res.json(error)
-          process.exit(1)
-     }
-})
+    const {description} = req.body
+    console.log(description)
+      if(!description){
+  throw new Error("Post description must be provided")
+      }
+
+    
+    const createdPost = await PostModel.create(req.body)
+        return res.json({
+           status:"success",
+           message:"Post created successfull",
+           createdPost
+        })
+    
+}))
 
 
 // geting all post 
@@ -62,58 +67,86 @@ app.get("/api/v1/posts", async(req,res) => {
 })
 
 // get a sing post
-app.get("/api/v1/posts/:postId",  async(req, res) => {
-  try {
-      // get post id
-  const {postId} = req.params
-  console.log(postId)
-  // get a single post by its id
-const postFound = await PostModel.findById(postId)
-if(!postFound){
-    throw new Error("No post found")
-}
-return res.json({
-    status:"success",
-    message:"Post found successfully",
-    postFound
-})
-
-  } catch (error) {
-    // throw new Error(error) 
-    console.log(error.message)
+app.get("/api/v1/posts/:postId",   asyncHandler(async(req, res) => {
+        // get post id
+    const {postId} = req.params
+    console.log(postId)
+    // get a single post by its id
+  const postFound = await PostModel.findById(postId)
+  if(!postFound){
+      throw new Error("No post found")
   }
-
-
-})
+  return res.json({
+      status:"success",
+      message:"Post found successfully",
+      postFound
+  })
+  
+    
+  
+  
+  }))
 
 
 //update a post
 
-app.put("/api/v1/posts/:postId", async(req, res) => {
-   try{
-const {title, description} = req.body
-    // get postId form params
-    console.log(req.body)
-    const {postId} = req.params
-const postFound = await PostModel.findById(postId);
-if(!postFound){
-    throw new Error("No post found")
-}
-// update post found
-const updatedPost = await PostModel.findByIdAndUpdate(postId, {
-    title: title ? title : postFound.title,
-    description: description ? description : postFound.description
-}, {
-    new: true
-})
- return res.json({
-    status:"success",
-    message:"Post updated successfully",
-    updatedPost
+app.put("/api/v1/posts/:postId", expressAsyncHandler(async(req, res) => {
+ const {title, description} = req.body
+     // get postId form params
+   
+     const {postId} = req.params
+ const postFound = await PostModel.findById(postId);
+ if(!postFound){
+     throw new Error("No post found")
+ }
+ // update post found
+ const updatedPost = await PostModel.findByIdAndUpdate(postId, {
+     title: title ? title : postFound.title,
+     description: description ? description : postFound.description
+ }, {
+     new: true
  })
-   }catch(error){
- throw new Error(error)
-   } 
+ 
+  return res.json({
+     status:"success",
+     message:"Post updated successfully",
+     updatedPost
+  })
+    
+ }))
+
+
+// delete post 
+
+app.delete("/api/v1/posts/:postId", asyncHandler( async(req, res) => {
+ const {postId} = req.params
+ const deletedPost = await PostModel.findByIdAndDelete(postId)
+if(!deletedPost){
+ throw new Error("Failed to delete post")
+}
+return res.json({
+    status:"success",
+    message:"Post deleted successfully",
+    deletedPost
+})
+   
+} ))
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message:"route not found"
+    })
+})
+
+app.use((err, req, res, next) => {
+    const errorMessage = err.message
+    // the stack proprty tells what area in the application the error happenz
+    const stack = err.stack
+res.status(500).json({
+    message: errorMessage,
+    stack
+})
+
 })
 
 const connetDbAndServer = async () => {
